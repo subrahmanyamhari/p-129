7d4e5bf23e3b20300a7eff4b1c4383623db8a72c
subrahmanyamhari <100850913+subrahmanyamhari@users.noreply.github.com>
2023-09-26 20:02:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import csv
 
 import pandas as pd
 
+import plotly_express as px
+
 df1 = pd.read_csv("scraped-data.csv")
 print(df1["mass"])
 
@@ -20,3 +22,13 @@ df1.to_csv("final_data.csv")
 
 df1["gravity"] = 6.6*10**-11 * df1["mass"]/df1["radius"]**2
 print(df1)
+df1.to_csv("final_data_with_gravity.csv")
+
+solar_chart_mass_radius = px.scatter(x=df1["mass"],y=df1["radius"])
+solar_chart_mass_radius.show()
+
+solar_chart_mass_gravity = px.scatter(x=df1["mass"],y=df1["gravity"])
+solar_chart_mass_gravity.show()
+
+solar_chart_gravity_radius = px.scatter(x=df1["gravity"],y=df1["radius"])
+solar_chart_gravity_radius.show()
